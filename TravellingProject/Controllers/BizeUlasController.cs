89dd539da9f3773bbc8f5633bc5f3a9df4394f76
burakhaslak <burakhaslak@DESKTOP-IV25BQJ>
2025-12-06 18:37:28 +0300

    Add project files.

---
+++ TravellingProject/Controllers/BizeUlasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravellingProject.Controllers
+{
+    public class BizeUlasController : Controller
+    {
+        // GET: BizeUlas
+        public ActionResult iletisim()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravellingProject/Controllers/RehberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravellingProject.Controllers
+{
+    public class RehberController : Controller
+    {
+        // GET: Rehber
+        public ActionResult hoverme()
+        {
+            return View();
+        }
+    }
+}
